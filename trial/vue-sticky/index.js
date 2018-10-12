529dfd26faf6e0ad4f76f7fb0b2d6f284cfabf9e
rulanfenghua <rulanfenghua@163.com>
2018-10-12 16:41:30 +0800

    sticky

--- trial/vue-sticky/index.js
+++ trial/vue-sticky/index.js
@@ -1,1 +1,22 @@
-index.js
+import throttle from './throttle'
+
+let listenAction
+let supportCSSStricky
+
+const getBindingConfig = binding => {
+	const params = binding.value || {}
+	let stickyTop = params.stickyTop || 0
+	let zIndex = params.zIndex || 1000
+	let disabled = params.disabled
+
+	return {stickyTop, zIndex, disabled}
+}
+const getInitialiConfig = el => {
+	return {zIndex: el.style.zIndex}
+}
+const unwatch = () => {
+	window.removeEventListener('scroll', listenAction)
+}
+const watch = () => {
+	window.addEventListener('scroll', listenAction)
+}
